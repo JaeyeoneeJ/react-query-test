206d0289404fb4a5814ad84c5dee876e9921811e
jaeyeoneej <5ikve@naver.com>
2024-02-13 17:58:55 +0900

    includes serviceApi on instance

--- src/api/api.js
+++ src/api/api.js
@@ -1,12 +1,9 @@
 import { getCurrentTime, getFormattedDate } from "../utils/utils";
 import { weatherInstance } from "./instance";
 
-const apiKey = process.env.REACT_APP_OPEN_API_KEY;
-
 export const getUltraSrtNcst = async () => {
   const res = await weatherInstance.get(`/getUltraSrtNcst`, {
     params: {
-      serviceKey: apiKey,
       numOfRows: "10",
       pageNo: "1",
       dataType: "JSON",
@@ -16,7 +13,7 @@ export const getUltraSrtNcst = async () => {
       ny: "127",
     },
   });
-  console.log("jjy res", res.data?.response?.body?.items?.item);
+
   if (res.data?.response?.header?.resultCode === "00") {
     return res.data?.response?.body?.items?.item;
   } else {
@@ -27,7 +24,6 @@ export const getUltraSrtNcst = async () => {
 export const getUltraSrtFcst = async () => {
   const res = await weatherInstance.get(`/getUltraSrtFcst`, {
     params: {
-      serviceKey: apiKey,
       numOfRows: "60",
       pageNo: "1",
       dataType: "JSON",
@@ -38,14 +34,12 @@ export const getUltraSrtFcst = async () => {
     },
   });
 
-  const sortRes = res.data?.response?.body?.items?.item?.sort(
+  const sortRes = [...res.data?.response?.body?.items?.item]?.sort(
     (a, b) => Number(a.fcstTime) - Number(b.fcstTime)
   );
-  console.log("jjy res", res.data?.response?.body?.items?.item);
-  console.log("jjy sortRes", sortRes);
 
   if (res.data?.response?.header?.resultCode === "00") {
-    return res.data?.response?.body?.items?.item;
+    return sortRes;
   } else {
     return [];
   }
@@ -54,7 +48,6 @@ export const getUltraSrtFcst = async () => {
 export const getVilageFcst = async () => {
   const res = await weatherInstance.get(`/getVilageFcst`, {
     params: {
-      serviceKey: apiKey,
       numOfRows: "60",
       pageNo: "1",
       dataType: "JSON",
@@ -65,14 +58,12 @@ export const getVilageFcst = async () => {
     },
   });
 
-  const sortRes = res.data?.response?.body?.items?.item?.sort(
+  const sortRes = [...res.data?.response?.body?.items?.item]?.sort(
     (a, b) => Number(a.fcstTime) - Number(b.fcstTime)
   );
-  console.log("jjy res", res.data?.response?.body?.items?.item);
-  console.log("jjy sortRes", sortRes);
 
   if (res.data?.response?.header?.resultCode === "00") {
-    return res.data?.response?.body?.items?.item;
+    return sortRes;
   } else {
     return [];
   }
--- src/api/instance.js
+++ src/api/instance.js
@@ -1,6 +1,11 @@
 import axios from "axios";
 
+const apiKey = process.env.REACT_APP_OPEN_API_KEY;
+
 // Axios 인스턴스 생성
 export const weatherInstance = axios.create({
   baseURL: process.env.REACT_APP_WEATHER_API_URL,
+  params: {
+    serviceKey: apiKey,
+  },
 });
