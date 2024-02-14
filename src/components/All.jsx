f0ca95cba8fd8d3c71fd15ef97280b763edd8107
jaeyeoneej <5ikve@naver.com>
2024-02-14 16:55:44 +0900

    used to useQueries

--- src/components/All.jsx
+++ src/components/All.jsx
@@ -3,19 +3,27 @@ import { useQueries } from "@tanstack/react-query";
 import { getUltraSrtFcst, getUltraSrtNcst, getVilageFcst } from "../api/api";
 
 const All = () => {
+  const queries = [
+    { queryKey: ["ultraSrcNcst"], queryFn: getUltraSrtNcst },
+    { queryKey: ["ultraSrcFcst"], queryFn: getUltraSrtFcst },
+    { queryKey: ["VilageFcst"], queryFn: getVilageFcst },
+  ];
   const results = useQueries({
-    queries: [
-      { queryKey: ["ultraSrcFcst"], queryFn: getUltraSrtFcst },
-      { queryKey: ["ultraSrcNcst"], queryFn: getUltraSrtNcst },
-      { queryKey: ["VilageFcst"], queryFn: getVilageFcst },
-    ],
-    combine: (results) => {
+    queries,
+    combine: (queryResults) => {
+      const combinedData = {};
+      queryResults.forEach((result, index) => {
+        const queryKey = queries[index].queryKey.join("_");
+        combinedData[queryKey] = result.data;
+      });
       return {
-        data: results.map((result) => result.data),
-        pending: results.some((result) => result.isPending),
+        data: combinedData,
+        pending: queryResults.some((result) => result.isPending),
       };
     },
   });
+
+  console.log("results: ", results);
 
   if (results.pending) {
     return <div>Not yet...</div>;
